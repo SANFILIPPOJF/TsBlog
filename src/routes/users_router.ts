b9dd45e0fe11e5843d0788d99ddfd1c8c57e852f
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2023-01-16 12:03:30 +0100

    start

---
+++ src/routes/users_router.ts
@@ -0,0 +1,9 @@
+import express = require('express');
+import { UsersController } from '../controllers/users_controller';
+
+export const usersRouter = express.Router();
+
+const usersController = new UsersController();
+
+usersRouter.post('/register', usersController.register)
+usersRouter.post('/login', usersController.login)
